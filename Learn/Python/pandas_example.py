1223830c14d09703203cba3239585919a30358c6
ChengBang Zhou <zhouchengbang@gmail.com>
2017-12-21 23:32:22 +0800

    jqdata test

--- Learn/Python/pandas_example.py
+++ Learn/Python/pandas_example.py
@@ -12,3 +12,26 @@ dates = pd.date_range('20130101',periods=6)
 print(dates)
 df = pd.DataFrame(np.random.randn(6,4),index=dates,columns=list('ABCD'))
 print(df)
+
+# 导入函数库
+#import jqdata
+
+# 初始化函数，设定基准等等
+def initialize(context):
+    security = '000001.XSHE'
+    start_date = '2016-07-01'
+    end_date = '2016-07-20'
+    frequency = 'daily'
+    fields = ['open','high','low','close']
+    df = get_price(security, start_date, end_date, frequency, fields)
+    print(df)
+    print('head')
+    print(df.head())
+    print('tail')
+    print(df.tail)
+    print(df.index)
+    print(df.values)
+    print(df.describe)
+    print(df.T)
+    df.sort(columns='open')
+    print(df)
